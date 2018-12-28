5a948c9b3db8007740ed2507e77eeb426480ab2b
Mike Wasauskee <m68476521.1@gmail.com>
2018-12-28 14:08:55 -0600

    Add initial project
    by m68476521

---
+++ WorldTrotter/WorldTrotter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by Miguel Orozco on 12/28/18.
+//  Copyright © 2018 m68476521. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
